9500c3ceed2159af63a504d461ab755989e5cb9f
Ken Lenhart <omnipaid@gmail.com>
2022-06-09 14:05:16 -0700

    Update hashes_1.rb

--- hashes_1.rb
+++ hashes_1.rb
@@ -1,30 +1,42 @@
 
-Activity:
+# Activity:
 
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your Terminal: 
+# file by entering the following command in your Terminal:
 # `ruby hashes_1.rb`
 
-foods = {"apples": 23, "grapes": 507, "eggs": 48}
-p zoo
+foods = {
+  "apples" => 23,
+  "grapes" => 507,
+  "eggs" => 48
+}
 
-# Write code that prints all of the 'keys' of the foods variable 
+# Write code that prints all of the 'keys' of the foods variable
 # you created above:
 # YOUR CODE HERE
 
-# Write code that prints all of the 'values' of the foods variable 
+p foods.keys
+
+
+
+# Write code that prints all of the 'values' of the foods variable
 # you created above:
 # YOUR CODE HERE
 
-# Write code that prints the value of the second food of the foods variable 
+p foods.values
+
+# Write code that prints the value of the second food of the foods variable
 # you created above:
 # YOUR CODE HERE
 
-# Write code that adds a food to the foods hash. 
+p foods["grapes"]
+
+# Write code that adds a food to the foods hash.
 # Then, print the updated hash:
 # YOUR CODE HERE
 
+foods["bananas"] = 10
 
 #-------------------
 # Part 2: Email
@@ -37,16 +49,31 @@ p zoo
 # Declare a variable that stores hash. Each key should be an attribute of an email and each
 # value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
 
+email = {
+  "sender" => ["Yahoo.com", "Mint.com", "Mom", "Steve"],
+  "subject" => ["World News", "Your Weekly Spending", "I made cookies!", "Hey man, how are you?"],
+  "inbox" => 220,
+  "sent" => 15,
+  "spam" => 554
+}
+
 # Write code that prints your email hash to the terminal.
 
+email.each do |key, value|
+  p "#{key} : #{value}"
+end
 
-# Write code that prints all of the 'keys' of the email hash 
+# Write code that prints all of the 'keys' of the email hash
 # you created above:
 # YOUR CODE HERE
 
-# Write code that prints all of the 'values' of the email hash 
+p email.keys
+
+# Write code that prints all of the 'values' of the email hash
 # you created above:
 # YOUR CODE HERE
+
+p email.values
 
 
 #-------------------
@@ -74,7 +101,7 @@ posts = [
     timestamp: "4:37 PM August 13, 2019",
     number_likes: 0,
     comments: []
-  }, 
+  },
   {
     image_src: "./images/holiday-party.png",
     caption: "What a great holiday party omg",
@@ -82,18 +109,43 @@ posts = [
     number_likes: 13,
     comments: []
   }
-]
 
 p posts
 p posts[0]
 
 
-# The code snippet above shows an Array with 2 elements. Each element in the Array is a 
-# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK 
+# The code snippet above shows an Array with 2 elements. Each element in the Array is a
+# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK
 # a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 # good to have some exposure before going into Mod 1.
 
 
-# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same 
+# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same
 # key-value pairs you used in your email Hash above.
 # Then, print the email Array to the Terminal.
+
+email = {
+  sender: ["Yahoo.com", "Mint.com", "Mom", "Steve"],
+  subject: ["World News", "Your Weekly Spending", "I made cookies!", "Hey man, how are you?"],
+  inbox: 220,
+  sent: 15,
+  spam: 554
+},
+{
+  sender: ["U-Haul.com", "Aunt May", "Dad", "Larry", "School"],
+  subject: ["Moving? We can Help!", "Recipe Idea", "Funny Article", "Picture of Fish I caught"],
+  inbox: 45,
+  sent: 0,
+  spam: 21
+},
+{
+  sender: ["Twitch.com", "Steam", "Netflix", "Bob"],
+  subject: ["Gaming News", "A Game is on Sale!", "New Shows!", "Hey man, how are you?"],
+  inbox: 55,
+  sent: 2,
+  spam: 99
+}
+
+email.each do |key, value|
+  p key, value
+end
